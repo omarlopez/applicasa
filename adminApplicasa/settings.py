a549a56e036d77f7eeff8b4e660514f5857c40d3
omarlopez <omarlaszloo@gmail.com>
2023-10-23 15:28:19 -0600

    update

--- adminApplicasa/settings.py
+++ adminApplicasa/settings.py
@@ -136,7 +136,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
