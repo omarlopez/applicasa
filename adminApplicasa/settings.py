484521ca995e769cbffcdefb6be32f20ff8a7989
omarlopez <omarlaszloo@gmail.com>
2023-10-23 21:43:54 -0600

    update

--- adminApplicasa/settings.py
+++ adminApplicasa/settings.py
@@ -147,8 +147,9 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
